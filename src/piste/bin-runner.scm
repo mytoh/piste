d922ca54d813f70c991db8577e2f8f5b26cdd1bd
mytoh <mytoh@gmail.com>
2012-09-22 11:11:40 +0900

    update

--- src/piste/bin-runner.scm
+++ src/piste/bin-runner.scm
@@ -8,24 +8,16 @@
   (use srfi-1)
   (require-extension (srfi 1 13))    ; iota
   (use kirjasto.tiedosto)
-  (use kirjasto.väri))
+  (use kirjasto.väri)
+
+  (use piste.util)
+  (use piste.commands))
 (select-module piste.bin-runner)
 
 (define piste-file (file->sexp-list (build-path (home-directory) ".pisterc")))
 (define *srcdir* (expand-path (caar piste-file)))
 (define *dotfiles* (cadar piste-file))
 
-
-;; util
-(define (path-home-file file)
-  (build-path (home-directory) file))
-
-(define (path-src-file file)
-  (build-path *srcdir* file))
-
-(define (print-list num lyst)
-  (for-each (^f (print (colour-string num f)))
-            lyst))
 
 ;; link
 (define (link-make-symlink files)
@@ -49,52 +41,12 @@
   (sys-symlink (path-src-file file)
                (path-home-file file)))
 
-;; list
-
-(define file-is-managed?
-  (every-pred
-    (^f (file-is-symlink? (path-home-file f)))
-    (^f (file-eqv? (sys-realpath (path-home-file f))
-                   (path-src-file f)))))
-
-(define (list-not-symlinks lyst)
-  (filter (^f (if (and (file-exists? (path-home-file f))
-                    (not (file-is-symlink? (path-home-file f))))
-                #t #f))
-          lyst))
-
-(define (list-symlinks lyst)
-  (filter (^f (if (file-is-symlink? (path-home-file f))
-                #t #f))
-          lyst))
-
-(define (list-managed-symlinks lyst)
-  (filter file-is-managed?
-          lyst))
-
-(define (list-not-managed-symlinks lyst)
-  (remove file-is-managed?  lyst))
-
-(define (list-dotfiles)
-  (let ((exist-files (list-not-symlinks *dotfiles*))
-        (managed-files (list-managed-symlinks *dotfiles*))
-        (not-managed-files (list-not-managed-symlinks *dotfiles*)))
-    (print "managed files")
-    (print-list 190 managed-files)
-    (newline)
-    (print "not symlink")
-    (print-list 38 exist-files)
-    (newline)
-    (print "not managed files")
-    (print-list 103 not-managed-files)))
 
 ;; commands
 
 (define (dot-link)
   (link-make-symlink *dotfiles*))
 
-(define (dot-list)
-  (list-dotfiles))
 
 
 (define (bin-runner args)
